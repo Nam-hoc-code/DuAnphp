6a5259b1cf11eee1e0119390ed1d3610c474cce3
Nam <leamle799@gmail.com>
2026-01-16 14:48:00 +0700

    add gitignore and update database config

--- amnhacwebtest/config/database.php
+++ amnhacwebtest/config/database.php
@@ -1,13 +1,19 @@
 <?php
 class Database {
-    protected $conn;
+    protected $conn = null;
 
     public function connect() {
         if ($this->conn === null) {
-            $this->conn = new mysqli("localhost", "root", "", "webmusicdb");
+            $this->conn = new mysqli(
+                "127.0.0.1",
+                "root",
+                "",
+                "webmusicdb",
+                3306
+            );
 
             if ($this->conn->connect_error) {
-                die("Kết nối thất bại: " . $this->conn->connect_error);
+                die("❌ Kết nối thất bại: " . $this->conn->connect_error);
             }
 
             $this->conn->set_charset("utf8mb4");
